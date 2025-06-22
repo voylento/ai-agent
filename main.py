a9158397e84a1452f51bee68ae0b67e1e2c1eccb
= <=>
2025-06-22 15:26:52 -0700

    save current changes to main.py

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from functions.run_python import schema_run_python_file
 from functions.write_file_content import schema_write_file
 from prompts import system_prompt
 
-MAX_CHARS = 10000
-
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
@@ -46,37 +44,50 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    iteration_count = 10
+
     try:
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions],
-                system_instruction=system_prompt,
-            ),
-        )
-
-        if verbose:
-            print(f"User prompt: {user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-        if not response.function_calls:
-            return response.text
-
-        for func_call in response.function_calls:
-            func_response = call_function(func_call)
-            if (
-                not func_response.parts
-                or not func_response.parts[0].function_response.response
-            ):
-                raise Exception(
-                    f"function response does not contain valid parts: {func_response}"
-                )
+        while iteration_count > 0:
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt,
+                ),
+            )
+            if response.candidates:
+                for candidate in response.candidates:
+                    messages.append(candidate.content)
 
             if verbose:
-                print(f"-> {func_response.parts[0].function_response.response}")
+                print(f"User prompt: {user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(
+                    f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                )
 
+            if not response.function_calls:
+                print(f"{response.text}")
+                return response.text
+
+            for func_call in response.function_calls:
+                func_response = call_function(func_call)
+                messages.append(func_response)
+                if (
+                    not func_response.parts
+                    or not func_response.parts[0].function_response.response
+                ):
+                    raise Exception(
+                        f"function response does not contain valid parts: {
+                            func_response
+                        }"
+                    )
+
+                if verbose:
+                    print(f"-> {func_response.parts[0].function_response.response}")
+
+            iteration_count -= 1
     except Exception as e:
         print(f"Exception: {e}")
 
